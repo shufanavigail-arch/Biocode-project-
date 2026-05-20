69d26aa95c3fcd63787dfed43316403b4b5a808c
shufanavigail-arch <shufan.avigail@gmail.com>
2026-05-20 18:21:22 +0300

    fixed division error

--- scripts/main.py
+++ scripts/main.py
@@ -8,7 +8,7 @@ def vector(file, list):
         if line[0] != '>':
             code_part += line
         if line[0] == '>' and code_part != "":
-            if len(code_part) % 3 == 0:
+            if len(code_part) % 3 != 0:
                 code_part = code_part[:len(code_part)-len(code_part)%3]
             for item in list:
                 codon_dict[item] = 0
@@ -26,21 +26,21 @@ def vector(file, list):
             code_part = ""
         
 
-        if len(code_part) % 3 == 0:
-            code_part = code_part[:len(code_part)-len(code_part)%3]
-        for item in list:
-            codon_dict[item] = 0
-        freq_list = []
-        RNA_seq = code_part.replace('T', 'U')
-        for i in range(0, len(RNA_seq), 3):
-            codon = RNA_seq[i:i+3]
-            if codon in list:
-                codon_dict[codon] += 1  
-        for item in list:
-            codon_dict[item] = codon_dict[item]/(len(RNA_seq)/3)
-            codon_dict[item] = round(codon_dict[item], 5)
-            freq_list.append(codon_dict[item])
-        database.append(freq_list)
+    if len(code_part) % 3 != 0:
+        code_part = code_part[:len(code_part)-len(code_part)%3]
+    for item in list:
+        codon_dict[item] = 0
+    freq_list = []
+    RNA_seq = code_part.replace('T', 'U')
+    for i in range(0, len(RNA_seq), 3):
+        codon = RNA_seq[i:i+3]
+        if codon in list:
+            codon_dict[codon] += 1  
+    for item in list:
+        codon_dict[item] = codon_dict[item]/(len(RNA_seq)/3)
+        codon_dict[item] = round(codon_dict[item], 5)
+        freq_list.append(codon_dict[item])
+    database.append(freq_list)
 
     return database
 
@@ -58,6 +58,7 @@ for line in codon_file:
     codon_list.append(line)
 
 print(vector(human_file, codon_list))
+print('\n')
 print(vector(ecoli_file, codon_list))
 
 codon_file.close()
